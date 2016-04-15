9c1c5ca3ec8302ced3fcddc210e472618802f2cd
MiGatoSeneca <pablo@citious.com>
2016-04-15 20:41:45 +0200

    added Access-Control-Allow-Methods

--- app.js
+++ app.js
@@ -52,6 +52,7 @@ var TYPES = require('tedious').TYPES;
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Key");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   next();
 });
 
